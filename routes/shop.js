f20c0c7ff8f7c39f3a668cf60578f867973db47f
hotsnow199 <49254639+hotsnow199@users.noreply.github.com>
2021-01-18 15:36:31 +0700

    set pug

--- routes/shop.js
+++ routes/shop.js
@@ -6,7 +6,11 @@ const router = express.Router();
 const displayProducts = admin.products;
 router.get('/', (_, res) => {
   console.log(displayProducts);
-  res.render('shop', { products: displayProducts, pageTitle: 'Shop Page' });
+  res.render('shop', {
+    products: displayProducts,
+    pageTitle: 'Shop Page',
+    hasProduct: displayProducts.length > 0,
+  });
 });
 
 module.exports = router;
